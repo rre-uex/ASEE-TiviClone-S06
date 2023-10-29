51a75e55a473f2ad5485c4ca86a1a46e2b6a5cff
RobertoRE <rrecheve@gmail.com>
2023-10-29 18:21:49 +0100

    exercise 1

---
+++ app/src/main/java/es/unex/giiis/asee/tiviclone/database/TiviCloneDatabase.kt
@@ -0,0 +1,33 @@
+package es.unex.giiis.asee.tiviclone.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import es.unex.giiis.asee.tiviclone.data.model.User
+import es.unex.giiis.asee.tiviclone.database.dao.UserDao
+
+@Database(entities = [User::class], version = 1)
+abstract class TiviCloneDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+
+    companion object {
+        private var INSTANCE: TiviCloneDatabase? = null
+
+        fun getInstance(context: Context): TiviCloneDatabase? {
+            if (INSTANCE == null) {
+                synchronized(TiviCloneDatabase::class) {
+                    INSTANCE = Room.databaseBuilder(
+                        context,
+                        TiviCloneDatabase::class.java, "tiviclone.db"
+                    ).build()
+                }
+            }
+            return INSTANCE
+        }
+
+        fun destroyInstance() {
+            INSTANCE = null
+        }
+    }
+}
